5be375da27aa4500f16bdc620058baaaeb6afb5d
DanPeled <danpeled31@gmail.com>
2023-01-10 21:17:23 +0200

    fixed a small issue with pull

--- Assets/Scripts/forms.js
+++ Assets/Scripts/forms.js
@@ -1,19 +1,31 @@
 let values = [];
-const submitValues = () => {
-    inputList = document.querySelectorAll("input");
-    selectList = document.querySelectorAll("select.info");
-    inputList.forEach((e, i) => {
+
+function listToDict(list){ // converts from string to json format.
+    var dict = {};
+    for (var i = 0; i < list.length; i++) {
+        var parts = list[i].split(': ');
+        var key = parts[0];
+        var value = parts[1];
+        dict[key] = value;
+        console.log(i);
+    }
+    return dict;
+}
+
+
+const addValues = (lst) => {
+    lst.forEach((e,i) => {
         values.push(`${inputList[i].getAttribute("placeholder")}: ${e.value}`);
     });
+}
 
-    selectList.forEach((e,i) => {
-        values.push(`${selectList[i].getAttribute("placeholder")}: ${e.value}`);
-    });
+const submitValues = () => {
+    values = []; // Resetting values
+    addValues(document.querySelectorAll("input"));
+    addValues(document.querySelectorAll("select.info"));
+    
     console.log(values);
-
-    //converts value to json format
     data = listToDict(values);
-
-    console.log(data);
-
 };
+
+
