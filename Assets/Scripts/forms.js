a13541538a006f86d1fd77221386e5e4406f7b90
DanPeled <danpeled31@gmail.com>
2023-01-10 16:46:14 +0200

    added new functions

--- Assets/Scripts/forms.js
+++ Assets/Scripts/forms.js
@@ -1,9 +1,23 @@
 const inputList = document.querySelectorAll("input.info");
 let values = [];
-const submitValues = () => {
-    inputList.forEach((e, i) => {
-        values[i] = e.value;
 
+const submitValues = async () => {
+    inputList.forEach((e) => {
+        values.push(e.value);
     });
-    console.log(values);
+    try {
+        const response = await fetch("https://danpeled.github.io/FTC-Scouting/", {
+            method: "POST",
+            body: JSON.stringify({ data: values }),
+            headers: { "Content-Type": "application/json" },
+        });
+        const data = await response.json();
+        if (data.success) {
+            console.log("File saved successfully");
+        } else {
+            console.error("Failed to save file");
+        }
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
 };
