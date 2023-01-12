3644ef5c828c5fe37aba2ba1e32f137688de4f65
DanPeled <danpeled31@gmail.com>
2023-01-12 19:19:34 +0200

    added firebase configuration

--- Assets/Scripts/firebase_script.js
+++ Assets/Scripts/firebase_script.js
@@ -7,13 +7,13 @@ import { getFirestore, doc, getDoc, setDoc, collection, addDoc, updateDoc, delet
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "", //insert apiKey
-  authDomain: "", //insert authDomain
-  projectId: "", //insert projectId
-  storageBucket: "", //insert storageBucket
-  messagingSenderId: "", //insert messagingSenderId
-  appId: "", //insert appId
-  measurementId: "" //insert measurementId
+  apiKey: "AIzaSyAnf5Hkdqhk5tNc1CLzOlaUz5VQnvHBSaY",
+  authDomain: "scouting-2023ftc.firebaseapp.com",
+  projectId: "scouting-2023ftc",
+  storageBucket: "scouting-2023ftc.appspot.com",
+  messagingSenderId: "1054772324553",
+  appId: "1:1054772324553:web:adb8956216bcb82a218518",
+  measurementId: "G-WD5T1X8NH3"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -24,22 +24,22 @@ console.log(app);
 // works, tested
 async function writeDoc__(collectionName, o_ = {}, id_ = "NONE") {
   const docRef = await setDoc(doc(db, collectionName, id_, "TEST23"), o_)
-  .then(() => {
-    console.log("Data set successfully");
-  }).catch((error) => {
-    console.log("Operation failed; error: " + error);
-  });
+    .then(() => {
+      console.log("Data set successfully");
+    }).catch((error) => {
+      console.log("Operation failed; error: " + error);
+    });
 }
 
 // works, tested
 async function getDocRef__(collectionName) {
-  if(collectionName.length % 2 == 0) {
+  if (collectionName.length % 2 == 0) {
     let docRef = doc(db, collectionName[0], collectionName[1]);
     for (let index = 2; index < collectionName.length; index += 2) {
       docRef = await doc(docRef, collectionName[index], collectionName[index + 1]);
     }
     return docRef;
-  } else if(collectionName.length % 2 == 1) {
+  } else if (collectionName.length % 2 == 1) {
     let docRef = doc(db);
     for (let index = 0; index < collectionName.length - 1; index += 2) {
       docRef = await doc(docRef, collectionName[index], collectionName[index + 1]);
@@ -70,19 +70,19 @@ async function getDocRef__(collectionName) {
 // works, tested;
 async function getDoc__(collectionName, id_) {
   const docSnap = await getDoc(doc(db, collectionName, id_));
-  if(docSnap.exists()) {
+  if (docSnap.exists()) {
     console.log("transmitting data");
     return docSnap.data();
   } else {
     console.log("File not found.");
-  } 
+  }
 }
-  
+
 // hasn't been tested yet
 async function deleteDocs__(collectionName, id_) {
   try {
     await deleteDoc(doc(db, collectionName, id_));
-  } catch(error) {
+  } catch (error) {
     console.log(error);
     return;
   }
