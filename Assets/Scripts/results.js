80cb4de96da80ee3899df47e286cc1714abd32ee
DanPeled <danpeled31@gmail.com>
2023-01-16 18:05:12 +0200

    name is displayed on top

--- Assets/Scripts/results.js
+++ Assets/Scripts/results.js
@@ -6,11 +6,22 @@ const resultViewer = document.getElementById("result_viewer");
 async function updateViewer(collectionPathArray) {
     let objs = await getDocsData__(collectionPathArray);
     resultViewer.innerHTML = "";
-    for(let i = 0; i < objs.length; i++) {
+    for (let i = 0; i < objs.length; i++) {
         let tempObjValues = Object.values(objs[i]);
         let tempObjKeys = Object.keys(objs[i]);
-        for(let i = tempObjKeys.length; i > 0; i--) {
-            resultViewer.innerHTML += `${tempObjKeys[i]}: ${tempObjValues[i]} <br>`;
+        let nameIndex = binarySearch(tempObjKeys, "name");
+        resultViewer.innerHTML += `${tempObjKeys[nameIndex]}: ${tempObjValues[nameIndex]} <br>`;
+
+        for (let w = tempObjKeys.length; w > 0; w--) {
+            if (tempObjKeys[w] != undefined && tempObjKeys[w] != "name") {
+                if (tempObjValues[w].toString() == "on") {
+                    tempObjValues[w] = "כן"
+                }
+                if (tempObjValues[w].toString() == "off") {
+                    tempObjValues[w] = "לא"
+                }
+                resultViewer.innerHTML += `${tempObjKeys[w].replaceAll("_", " ")}: ${tempObjValues[w]} <br>`;
+            }
             console.log(resultViewer.innerHTML);
         }
         resultViewer.innerHTML += "<br><br>";
@@ -26,4 +37,24 @@ window.addEventListener("load", eve => {
 
 
 
+
+
+function binarySearch(array, target) {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+
+        if (array[middle] === target) {
+            return middle;
+        } else if (array[middle] < target) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+
+    return -1;
+}
 
